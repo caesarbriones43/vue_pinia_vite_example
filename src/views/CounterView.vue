172fa9d89015d1b7112e2d10972bb86d81da3b6d
Caesar Briones <67029455+caesarbriones43@users.noreply.github.com>
2022-06-27 03:49:32 -0500

    vue_pinia_vite_example
    vue_pinia_vite_example

---
+++ src/views/CounterView.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Counter View : {{ counterStore.counter }}</h1>
+
+  <button @click="increaseBy(1)">+1</button>
+  <button @click="increaseBy(2)">+2</button>
+  <button @click="increaseBy(5)">+5</button>
+  <button @click="counterStore.incrementBy(10)">+10</button>
+</template>
+
+<script lang="ts" setup>
+import { userCounterStore } from "../stores/counter";
+
+const counterStore = userCounterStore();
+
+const increaseBy = (value: number) => {
+  counterStore.incrementBy(value);
+};
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>HomeView : {{counterStore.counter}}</h1>
+  <h2>Veces Cambiada: {{counterStore.numberOfChanges}}</h2>
+</template>
+
+<script lang="ts" setup>
+import { userCounterStore } from '../stores/counter';
+
+const counterStore = userCounterStore();
+</script>
+
+<style></style>
